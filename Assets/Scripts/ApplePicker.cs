c7e9223372e20cd6cd0ca88a1e10499a693d7b17
Lydia Ha <lyh12@scarletmail.rutgers.edu>
2018-06-12 17:44:16 -0400

    Instantiating Baskets - created 3 baskets to catch dropping apples

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ApplePicker : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	[Header("Set in Inspector")]
 		
+	public GameObject basketPrefab;
+
+	public int numBaskets = 3;
+
+	public float basketBottomY = -14f;
+
+	public float basketSpacingY = 2f;
+
+	void Start () {
+		for (int i=0; i<numBaskets; i++) {
+			GameObject tBasketGO = Instantiate<GameObject> (basketPrefab);
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + (basketSpacingY * i);
+			tBasketGO.transform.position = pos;
+		}
 	}
 	
 	// Update is called once per frame
