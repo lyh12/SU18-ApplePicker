63c357f15607c6924be6ef030c85d2cd98d96e39
Lydia Ha <lyh12@scarletmail.rutgers.edu>
2018-06-12 19:28:29 -0400

    Notifying Applepicker that an apple was dropped - destroying a basket when an apple drops

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Apple : MonoBehaviour {
-	public static float bottomY = -20f;
+	[Header ("Set in Inspector")]
+
+	public static float bottomY = -20f; 
 	// Use this for initialization
 
 	void Start () {
@@ -12,8 +14,12 @@ public class Apple : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.y < bottomY)
+		if (transform.position.y < bottomY) {
 			Destroy (this.gameObject);
+
+		ApplePicker apScript = Camera.main.GetComponent <ApplePicker> ();
+		apScript.AppleDestroyed ();
 		
 	}
+}
 }
